81e79fe304a9f2e335366b9121e3c4aade810a80
threonyl <selim.kirbiyik@sabanciuniv.edu>
2024-11-07 01:36:41 +0300

    removed old file, added new 3 term parametric k2red

---
+++ src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift.v
@@ -0,0 +1,87 @@
+module k2red_ln_shift #(parameter W = 64) (
+  input                        clk,
+  input                        rst,
+  input      [      (2*W)-1:0] A  ,
+  input      [          W-1:0] Q  ,
+  input      [$clog2(2*W)-1:0] l1 ,
+  input      [$clog2(2*W)-1:0] l2 ,
+  input      [$clog2(2*W)-1:0] l3 ,
+  input      [$clog2(2*W)-1:0] m  , // We can fix m to get better synthesis results :)
+  output reg [          W-1:0] C2
+);
+
+  reg  signed [(2*W)-1:0] C1 ;
+  wire        [(2*W)-1:0] C1L;
+  wire        [(2*W)-1:0] C1H;
+  wire        [(2*W)-1:0] AH   ;
+  wire        [(2*W)-1:0] AL   ;
+  reg  signed [(2*W)-1:0] C2int;
+
+  barrelmask d00 (clk,rst,A,m,AH,AL);
+  barrelmask d01 (clk,rst,C1,m,C1H,C1L);
+
+  always @(posedge clk )
+    begin
+      if (rst)
+        begin
+          C2    <= 0;
+          C1    <= 0;
+          C2int <= 0;
+        end
+      else
+        begin
+          C1    <= ((AL  << (W-1-m)) + (AL  << l1) + (AL  << l3)) - ((AL  << l2) + AH);
+          C2int <= ((C1L << (W-1-m)) + (C1L << l1) + (C1L << l3)) - ((C1L << l2) + C1H);
+          // 3rd stage
+          if ((C2int > Q) || (C2int == Q))
+            begin
+              C2 <= C2int - Q;
+            end
+          else
+            begin
+              if (C2int < 0) begin
+                C2 <= C2int + Q;
+              end else begin
+                C2 <= C2int;
+              end
+            end
+        end
+    end
+
+  `ifdef COCOTB_SIM
+    initial
+      begin
+        $dumpfile ("k2red.vcd");
+        $dumpvars (0, k2red_ln_shift);
+      end
+  `endif
+
+endmodule
+
+
+module barrelmask #(parameter W = 64) (
+  input                             clk   ,
+  input                             rst   ,
+  input      signed [        W-1:0] A     ,
+  input             [$clog2(W)-1:0] select,
+  output reg signed [        W-1:0] AH    , // this might be negative
+  output reg        [        W-1:0] AL
+);
+
+  reg signed [W-1:0] mask   ;
+  reg        [W-1:0] midmask;
+
+  always @(posedge clk) begin
+    if (rst) begin
+      AH      <= 0;
+      AL      <= 0;
+      mask    <= (1 << W-1);
+      midmask <= 0;
+    end else begin
+      midmask <= (mask >>> (W-select-1));
+      AH      <= A >>> select;
+      AL      <= A & ~midmask;
+    end
+  end
+
+endmodule
---
+++ src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift_tb.v
@@ -0,0 +1,74 @@
+module k2red_ln_shift_tb();
+
+// Parameters
+localparam  W = 32;
+
+parameter HP = 5;
+parameter FP = (2*HP);
+
+reg clk;
+reg rst;
+
+reg [63:0] A;
+reg [31:0] Q;
+reg [5:0] l1;
+reg [5:0] l2;
+reg [5:0] l3;
+reg [5:0] m;
+wire [31:0] C2;
+
+k2red_ln_shift # (
+    .W(W)
+  )
+  dut (
+    .clk(clk),
+    .rst(rst),
+    .A(A),
+    .Q(Q),
+    .l1(l1),
+    .l2(l2),
+    .l3(l3),
+    .m(m),
+    .C2(C2)
+  );
+
+always #HP clk = ~clk;
+
+initial begin
+    $dumpfile("k2red.vcd"); 
+
+    $dumpvars(0,k2red_ln_shift_tb);  // * Get the variables from the module.
+
+    $display("Simulation started.");
+    //
+    clk = 0;
+    rst = 1;
+    A = 0;
+    Q = 0;
+    l1 = 0;
+    l2 = 0;
+    l3 = 0;
+    m = 0;
+    #FP;
+    rst = 0;
+    A = 63'd2500883870215315764; // k^2*C mod q =  1965696994
+    Q = 32'd2148794369;
+    l1 = 2;
+    l2 = 1;
+    l3 = 3;
+    m = 17;
+    #FP;
+    rst = 0;
+    #(5*FP);
+    if (C2 == 32'd1965696994) begin
+        $display("Nice :)");
+    end else begin
+        $display("No :(");
+    end
+    //
+    $display("Simulation finished."); 
+    $finish(); // Finish simulation.
+end
+
+
+endmodule
