57c01a7e8c2e94e7c04a808cafb90dcf0e86345c
threonyl <selim.kirbiyik@sabanciuniv.edu>
2024-11-08 15:55:45 +0300

    Corrected assignments as wire

--- src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift.v
+++ src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift.v
@@ -2,31 +2,35 @@
 // #(parameter LOG_Q = 64, M = 46, LOG_L = 4)
 
 module k2red_ln_shift #(parameter LOG_Q = 32, M = 17, LOG_L = 4, USE_L3 = 1, SPEED_OPT = 1) (
-  input                  clk,
+  input                      clk,
   input      [(2*LOG_Q)-1:0] A  ,
   input      [    LOG_Q-1:0] Q  ,
-  input      [LOG_L-1:0] l1 ,
-  input      [LOG_L-1:0] l2 ,
-  input      [LOG_L-1:0] l3 ,
+  input      [    LOG_L-1:0] l1 ,
+  input      [    LOG_L-1:0] l2 ,
+  input      [    LOG_L-1:0] l3 ,
   output reg [    LOG_Q-1:0] C2
 );
 
-  localparam L_MAX  = (1 << LOG_L) - 1;
-  localparam LOG_C1 = (2*LOG_Q) - M + 1   ;
-  localparam DELAY = 5 + (2*SPEED_OPT);
-  
-  reg [LOG_Q-1:0] q_pipeline [DELAY-1:0];
+  localparam L_MAX  = (1 << LOG_L) - 1 ;
+  localparam LOG_C1 = (2*LOG_Q) - M + 1;
+  localparam DELAY  = 5 + (2*SPEED_OPT);
 
-  reg  [(2*LOG_Q)-M-1:0] AH,AH_q,AH_mx;
-  reg  [      M-1:0] AL,AL_q,AL_mx;
-  wire [L_MAX+M-1:0] ALw1m;
-  wire [L_MAX+M-1:0] ALl1,ALl2,ALl3;
-  reg  [L_MAX+M-1:0] ALl1_q,ALl2_q,ALl3_q;
-  wire [L_MAX+M-1:0] ALl1_mx,ALl2_mx,ALl3_mx;
+  reg [LOG_Q-1:0] q_pipeline[DELAY-1:0];
 
-  reg signed [LOG_C1-1:0] C1 ;
-  reg        [M-1:0] C1L, C1L_q, C1L_mx;
-  reg signed [LOG_C1-M-1:0] C1H, C1H_q, C1H_mx;
+  reg  [(2*LOG_Q)-M-1:0] AH,AH_q;
+  wire [(2*LOG_Q)-M-1:0] AH_mx  ;
+  reg  [          M-1:0] AL,AL_q;
+  wire [          M-1:0] AL_mx  ;
+  wire [    L_MAX+M-1:0] ALw1m  ;
+  wire [    L_MAX+M-1:0] ALl1,ALl2,ALl3;
+  reg  [    L_MAX+M-1:0] ALl1_q,ALl2_q,ALl3_q;
+  wire [    L_MAX+M-1:0] ALl1_mx,ALl2_mx,ALl3_mx;
+
+  reg  signed [  LOG_C1-1:0] C1    ;
+  reg         [       M-1:0] C1L, C1L_q;
+  wire        [       M-1:0] C1L_mx;
+  reg  signed [LOG_C1-M-1:0] C1H,C1H_q;
+  wire signed [LOG_C1-M-1:0] C1H_mx;
 
   wire signed [L_MAX+M-1:0] C1w1m,C1l1,C1l2,C1l3;
   reg  signed [L_MAX+M-1:0] C1l1_q,C1l2_q,C1l3_q;
@@ -49,17 +53,17 @@ module k2red_ln_shift #(parameter LOG_Q = 32, M = 17, LOG_L = 4, USE_L3 = 1, SPE
   assign C1l3  = (USE_L3) ? C1L << l3 : 0;
 
 
-  assign AH_mx    = (SPEED_OPT) ? AH_q    : AH;
-  assign AL_mx    = (SPEED_OPT) ? AL_q    : AL;
-  assign ALl1_mx  = (SPEED_OPT) ? ALl1_q  : ALl1;
-  assign ALl2_mx  = (SPEED_OPT) ? ALl2_q  : ALl2;
-  assign ALl3_mx  = (SPEED_OPT) ? ALl3_q  : ALl3;
+  assign AH_mx   = (SPEED_OPT) ? AH_q    : AH;
+  assign AL_mx   = (SPEED_OPT) ? AL_q    : AL;
+  assign ALl1_mx = (SPEED_OPT) ? ALl1_q  : ALl1;
+  assign ALl2_mx = (SPEED_OPT) ? ALl2_q  : ALl2;
+  assign ALl3_mx = (SPEED_OPT) ? ALl3_q  : ALl3;
 
-  assign C1L_mx = (SPEED_OPT) ? C1L_q   : C1L;
-  assign C1H_mx = (SPEED_OPT) ? C1H_q   : C1H;
-  assign C1l1_mx  = (SPEED_OPT) ? C1l1_q  : C1l1;
-  assign C1l2_mx  = (SPEED_OPT) ? C1l2_q  : C1l2;
-  assign C1l3_mx  = (SPEED_OPT) ? C1l3_q  : C1l3;
+  assign C1L_mx  = (SPEED_OPT) ? C1L_q   : C1L;
+  assign C1H_mx  = (SPEED_OPT) ? C1H_q   : C1H;
+  assign C1l1_mx = (SPEED_OPT) ? C1l1_q  : C1l1;
+  assign C1l2_mx = (SPEED_OPT) ? C1l2_q  : C1l2;
+  assign C1l3_mx = (SPEED_OPT) ? C1l3_q  : C1l3;
 
 
   generate
@@ -72,12 +76,12 @@ module k2red_ln_shift #(parameter LOG_Q = 32, M = 17, LOG_L = 4, USE_L3 = 1, SPE
           ALl2_q  <= ALl2;
           ALl3_q  <= ALl3;
         end
-    end
-  endgenerate
+      end
+    endgenerate
 
-  generate
-    if (SPEED_OPT) begin
-      always @(posedge clk )
+    generate
+      if (SPEED_OPT) begin
+        always @(posedge clk )
         begin
           C1L_q   <= C1L;
           C1H_q   <= C1H;
@@ -88,17 +92,17 @@ module k2red_ln_shift #(parameter LOG_Q = 32, M = 17, LOG_L = 4, USE_L3 = 1, SPE
       end
     endgenerate
 
-  generate
-    for(genvar dly=0; dly < DELAY; dly=dly+1)
-    begin
-        always @(posedge clk)
+    generate
+      for(genvar dly=0; dly < DELAY; dly=dly+1)
         begin
-          q_pipeline[dly] <= (dly == 0) ? Q : q_pipeline[dly - 1];
+          always @(posedge clk)
+          begin
+            q_pipeline[dly] <= (dly == 0) ? Q : q_pipeline[dly - 1];
+          end
         end
-    end
-  endgenerate
+    endgenerate
 
-  always @(posedge clk )
+    always @(posedge clk )
     begin
       // 1st stage
       AH <= A[(2*LOG_Q)-1:M]; // Get (LOG_Q-M)-bit MSBs
