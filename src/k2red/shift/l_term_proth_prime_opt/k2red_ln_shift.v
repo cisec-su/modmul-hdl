c0904f5be9728200b77f3830b05714850e6a655d
threonyl <selim.kirbiyik@sabanciuniv.edu>
2024-11-11 04:32:20 +0300

    k2red_ln_shift, verified with 32,64 bits, verified pipeline, added valid_in/out

--- src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift.v
+++ src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift.v
@@ -1,57 +1,66 @@
-// #(parameter LOG_Q = 32, M = 17, LOG_L = 4)
-// #(parameter LOG_Q = 64, M = 46, LOG_L = 4)
+// #(parameter LOG_Q = 32, M = 17, LOG_L = 4, USE_L3 = 1, SPEED_OPT = 1)
+// #(parameter LOG_Q = 64, M = 47, LOG_L = 4, USE_L3 = 1, SPEED_OPT = 1)
 
-module k2red_ln_shift #(parameter LOG_Q = 64, M = 47, LOG_L = 4, USE_L3 = 1, SPEED_OPT = 1) (
-  input                      clk,
-  input      [(2*LOG_Q)-1:0] A  ,
-  input      [    LOG_Q-1:0] Q  ,
-  input      [    LOG_L-1:0] l1 ,
-  input      [    LOG_L-1:0] l2 ,
-  input      [    LOG_L-1:0] l3 ,
-  output reg [    LOG_Q-1:0] C2
+module k2red_ln_shift #(parameter LOG_Q = 32, M = 17, LOG_L = 4, USE_L3 = 1, SPEED_OPT = 1) (
+  input                      clk      ,
+  input      [(2*LOG_Q)-1:0] A        ,
+  input      [    LOG_Q-1:0] Q        ,
+  input      [    LOG_L-1:0] l1       ,
+  input      [    LOG_L-1:0] l2       ,
+  input      [    LOG_L-1:0] l3       ,
+  input                      valid_in ,
+  output reg [    LOG_Q-1:0] C2       ,
+  output                     valid_out
 );
 
-  localparam L_MAX  = (1 << LOG_L) - 1 ;
-  localparam LOG_C1 = (2*LOG_Q) - M + 1;
-  localparam DELAY  = 5 + (2*SPEED_OPT);
+  localparam L_MAX = (1 << LOG_L) - 1 ; // Maximum shift amount for Ls
+  localparam LOG_C = (2*LOG_Q) - M + 1; // Bit-width for C1 and C2 (+1 for sign bit)
+  localparam DELAY = 5 + (2*SPEED_OPT); // Latency of the module
 
-  reg [LOG_Q-1:0] q_pipeline[DELAY-1:0];
+  // Pipeline registers for stream processing
+  reg [LOG_Q-1:0] q_pipeline    [DELAY-1:0];
+  reg [LOG_L-1:0] l1_pipeline   [DELAY-1:0];
+  reg [LOG_L-1:0] l2_pipeline   [DELAY-1:0];
+  reg [LOG_L-1:0] l3_pipeline   [DELAY-1:0];
+  reg             valid_pipeline[  DELAY:0];
 
   reg  [(2*LOG_Q)-M-1:0] AH,AH_q;
-  wire [(2*LOG_Q)-M-1:0] AH_mx  ;
-  reg  [          M-1:0] AL,AL_q;
-  wire [          M-1:0] AL_mx  ;
+  wire [(2*LOG_Q)-M-1:0] AH_mx  ; // Pipeline mux output
+  reg  [          M-1:0] AL,AL_q; // Pipeline reg output
+  wire [          M-1:0] AL_mx  ; // Pipeline mux output
   wire [    L_MAX+M-1:0] ALw1m  ;
   wire [    L_MAX+M-1:0] ALl1,ALl2,ALl3;
-  reg  [    L_MAX+M-1:0] ALl1_q,ALl2_q,ALl3_q;
-  wire [    L_MAX+M-1:0] ALl1_mx,ALl2_mx,ALl3_mx;
+  reg  [    L_MAX+M-1:0] ALl1_q,ALl2_q,ALl3_q; // Pipeline reg output
+  wire [    L_MAX+M-1:0] ALl1_mx,ALl2_mx,ALl3_mx; // Pipeline mux output
 
-  reg  signed [  LOG_C1-1:0] C1    ;
-  reg  signed [       M-1:0] C1L, C1L_q;
-  wire signed [       M-1:0] C1L_mx;
-  reg  signed [LOG_C1-M-1:0] C1H,C1H_q;
-  wire signed [LOG_C1-M-1:0] C1H_mx;
+  reg  signed [  LOG_C-1:0] C1    ;
+  reg  signed [        M:0] C1L, C1L_q;
+  wire signed [        M:0] C1L_mx;
+  reg  signed [LOG_C-M-1:0] C1H,C1H_q;
+  wire signed [LOG_C-M-1:0] C1H_mx;
 
-  wire signed [L_MAX+M-1:0] C1w1m,C1l1,C1l2,C1l3;
-  reg  signed [L_MAX+M-1:0] C1l1_q,C1l2_q,C1l3_q;
-  wire signed [L_MAX+M-1:0] C1l1_mx,C1l2_mx,C1l3_mx;
+  wire signed [L_MAX+M-1:0] C1w1m,C1l1,C1l2,C1l3; // Added 1 bit for sign
+  reg  signed [L_MAX+M-1:0] C1l1_q,C1l2_q,C1l3_q; // Added 1 bit for sign
+  wire signed [L_MAX+M-1:0] C1l1_mx,C1l2_mx,C1l3_mx; // Added 1 bit for sign
 
 
-  reg signed [(2*LOG_Q)-1:0] C2int;
+  reg signed [LOG_C-1:0] C2int;
+
 
   // Barrel Shifters
   // A Shift Pipeline Steps
   assign ALw1m = AL_mx << (LOG_Q-1-M);
-  assign ALl1  = AL << l1;
-  assign ALl2  = AL << l2;
-  assign ALl3  = (USE_L3) ? AL << l3 : 0;
+  assign ALl1  = AL << l1_pipeline[0];
+  assign ALl2  = AL << l2_pipeline[0];
+  assign ALl3  = (USE_L3) ? AL << l3_pipeline[0] : 0;
 
 // C1 Shift Pipeline Steps
   assign C1w1m = C1L_mx << (LOG_Q-1-M);
-  assign C1l1  = C1L << l1;
-  assign C1l2  = C1L << l2;
-  assign C1l3  = (USE_L3) ? C1L << l3 : 0;
+  assign C1l1  = C1L << l1_pipeline[2+SPEED_OPT];
+  assign C1l2  = C1L << l2_pipeline[2+SPEED_OPT];
+  assign C1l3  = (USE_L3) ? C1L << l3_pipeline[2+SPEED_OPT] : 0;
 
+// Pipeline Multiplexers
 
   assign AH_mx   = (SPEED_OPT) ? AH_q    : AH;
   assign AL_mx   = (SPEED_OPT) ? AL_q    : AL;
@@ -65,6 +74,7 @@ module k2red_ln_shift #(parameter LOG_Q = 64, M = 47, LOG_L = 4, USE_L3 = 1, SPE
   assign C1l2_mx = (SPEED_OPT) ? C1l2_q  : C1l2;
   assign C1l3_mx = (SPEED_OPT) ? C1l3_q  : C1l3;
 
+  assign valid_out = valid_pipeline[DELAY-1];
 
   generate
     if (SPEED_OPT) begin
@@ -102,6 +112,28 @@ module k2red_ln_shift #(parameter LOG_Q = 64, M = 47, LOG_L = 4, USE_L3 = 1, SPE
         end
     endgenerate
 
+    generate
+      for(genvar dly_l=0; dly_l < DELAY-(2+(SPEED_OPT))+1; dly_l=dly_l+1)
+        begin
+          always @(posedge clk)
+          begin
+            l1_pipeline[dly_l] <= (dly_l == 0) ? l1 : l1_pipeline[dly_l - 1];
+            l2_pipeline[dly_l] <= (dly_l == 0) ? l2 : l2_pipeline[dly_l - 1];
+            l3_pipeline[dly_l] <= (dly_l == 0) ? l3 : l3_pipeline[dly_l - 1];
+          end
+        end
+    endgenerate
+
+    generate
+      for(genvar dly_v=0; dly_v < DELAY; dly_v=dly_v+1)
+        begin
+          always @(posedge clk)
+          begin
+            valid_pipeline[dly_v] <= (dly_v == 0) ? valid_in : valid_pipeline[dly_v - 1];
+          end
+        end
+    endgenerate
+
     always @(posedge clk )
     begin
       // 1st stage
@@ -109,18 +141,18 @@ module k2red_ln_shift #(parameter LOG_Q = 64, M = 47, LOG_L = 4, USE_L3 = 1, SPE
       AL <= A[M    -1:0]; // Get M-bit LSBs
       // 2nd stage
       if (USE_L3) begin
-        C1 <= ((ALw1m) + (ALl1_mx) + (ALl3_mx)) - ((ALl2_mx) + AH_mx); // First k*A_L - A_H
+        C1 <= ((ALw1m) + (ALl1_mx) + (ALl3_mx)) - ((ALl2_mx) + {AH_mx}); // First k*A_L - A_H --> (1cc/2cc SPEED_OPT)
       end else begin
-        C1 <= ((ALw1m) + (ALl1_mx)) - ((ALl2_mx) + AH_mx); // First k*A_L - A_H
+        C1 <= ((ALw1m) + (ALl1_mx)) - ((ALl2_mx) + {AH_mx}); // First k*A_L - A_H
       end
       // 3rd stage
-      C1H <= C1[LOG_C1-1:M]; // Get (2W-M) MSBs
-      C1L <= C1[M-1:0];
+      C1H <= C1[LOG_C-1:M];
+      C1L <= {1'b0,C1[M-1:0]};
       // 4th stage
       if (USE_L3) begin
-        C2int <= ((C1w1m) + (C1l1_mx) + (C1l3_mx)) - ((C1l2_mx) + C1H_mx); // Second k*C_L - C_H (4 Shift)
+        C2int <= ((C1w1m) + (C1l1_mx) + (C1l3_mx)) - ((C1l2_mx) + (C1H_mx)); // Second k*C_L - C_H --> 1cc/2cc (SPEED_OPT)
       end else begin
-        C2int <= ((C1w1m) + (C1l1_mx)) - ((C1l2_mx) + C1H_mx); // Second k*C_L - C_H (4 Shift)
+        C2int <= ((C1w1m) + (C1l1_mx)) - ((C1l2_mx) + (C1H_mx)); // Second k*C_L - C_H
       end
 
       // 5th stage
--- src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift_tb.v
+++ src/k2red/shift/l_term_proth_prime_opt/k2red_ln_shift_tb.v
@@ -3,12 +3,12 @@ module k2red_ln_shift_tb();
 `define USE_32_BITS
 
 `ifdef USE_32_BITS
-localparam W = 32;
+localparam LOG_Q = 32;
 localparam M = 17;
 localparam LOG_L = 4;
 `else
-localparam W = 64;
-localparam M = 46;
+localparam LOG_Q = 64;
+localparam M = 47;
 localparam LOG_L = 4;
 `endif
 
@@ -20,15 +20,17 @@ parameter FP = (2*HP);
 reg clk;
 reg rst;
 
-reg [(2*W)-1:0] A;
-reg [W-1:0] Q;
+reg [(2*LOG_Q)-1:0] A;
+reg [LOG_Q-1:0] Q;
 reg [LOG_L-1:0] l1;
 reg [LOG_L-1:0] l2;
 reg [LOG_L-1:0] l3;
-wire [W-1:0] C2;
+reg valid_in;
+wire [LOG_Q-1:0] C2;
+wire valid_out;
 
 k2red_ln_shift # (
-    .W(W),
+    .LOG_Q(LOG_Q),
     .M(M),
     .LOG_L(LOG_L),
     .USE_L3(USE_L3),
@@ -41,7 +43,9 @@ k2red_ln_shift # (
     .l1(l1),
     .l2(l2),
     .l3(l3),
-    .C2(C2)
+    .valid_in(valid_in),
+    .C2(C2),
+    .valid_out(valid_out)
   );
 
 always #HP clk = ~clk;
@@ -59,6 +63,7 @@ initial begin
     l1 = 0;
     l2 = 0;
     l3 = 0;
+    valid_in = 0;
     #FP;
     `ifdef USE_32_BITS
     A = 63'd2500883870215315764; // k^2*C mod q =  1965696994
@@ -66,8 +71,14 @@ initial begin
     l1 = 2;
     l2 = 1;
     l3 = 3;
-    #FP;
-    rst = 0;
+    valid_in = 1;
+    #FP; // Verify that it works pipelined
+    A = 0;
+    Q = 0;
+    l1 = 0;
+    l2 = 0;
+    l3 = 0;
+    valid_in = 0;
     #((4+(SPEED_OPT*2))*FP);
     if (C2 == 32'd1965696994) begin
         $display("Correct :)");
@@ -80,8 +91,14 @@ initial begin
     l1 = 2;
     l2 = 1;
     l3 = 13;
-    #FP;
-    rst = 0;
+    valid_in = 1;
+    #FP; // Verify that it works pipelined
+    A = 0;
+    Q = 0;
+    l1 = 0;
+    l2 = 0;
+    l3 = 0;
+    valid_in = 0;
     #((4+(SPEED_OPT*2))*FP);
     if (C2 == 64'd6509854724) begin
         $display("Correct :)");
