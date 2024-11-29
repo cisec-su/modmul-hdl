2fb9c99668aedabf3f446bd58b53802d22e3ba07
Tolun Tosun <toluntosun@sabanciuni.edu>
2024-11-29 17:44:33 +0300

    Montgomery shift tested

--- src/monty/montgomery_shift.sv
+++ src/monty/montgomery_shift.sv
@@ -2,8 +2,8 @@
 
 /*
 
-  Q    = 2^(LOGQ-1) + (2^K1 - 2^K2 + 2^K3)*2^M + 1
- -Q^-1 = 2^(LOGQ-1) + (2^K1 - 2^K2 + 2^K3)*2^M - 1
+  Q    = 2^(LOGQ-1) + (2^L1 - 2^L2 + 2^L3)*2^M + 1
+ -Q^-1 = 2^(LOGQ-1) + (2^L1 - 2^L2 + 2^L3)*2^M - 1
 
 */
 
@@ -11,10 +11,10 @@ module montgomery_shift
    #(
         parameter  LOGQ    = 64,
         parameter  LOGQH   = 32,
-        parameter  LOGK1   = 5 ,
-        parameter  LOGK2   = 5 ,
-        parameter  USE_K3  = 1 ,
-        parameter  LOGK3   = 5 ,
+        parameter  LOGL1   = 5 ,
+        parameter  LOGL2   = 5 ,
+        parameter  USE_L3  = 1 ,
+        parameter  LOGL3   = 5 ,
         parameter  CORRECT = 1 ,
         parameter  FF_IN   = 1 ,
         parameter  FF_SHF0 = 1 ,
@@ -28,22 +28,23 @@ module montgomery_shift
         input               clk,
         input               rst,
         input  [LOGQ*2-1:0] C  ,
-        output [LOGQ  -1:0] T  ,
+        output [LOGT  -1:0] T  ,
         input  [LOGQH -1:0] qH ,
-        input  [LOGK1 -1:0] K1 ,
-        input  [LOGK2 -1:0] K2 ,
-        input  [LOGK3 -1:0] K3
+        input  [LOGL1 -1:0] L1 ,
+        input  [LOGL2 -1:0] L2 ,
+        input  [LOGL3 -1:0] L3
     );
 
 ///////////////////////////// parameters ////////////////////////////////
 
+localparam LOGT = (CORRECT) ? LOGQ : LOGQ + 1;
 localparam montgomery_shift_params_t params = {CORRECT, FF_IN, FF_SHF0, FF_SUB0, FF_SHF1, FF_SUB1, FF_SUM, FF_OUT};
 localparam M = LOGQ - LOGQH;
 localparam LAT = montgomery_shift_lat(params);
 localparam LAT_1 = (CORRECT) ? LAT - montgomery_shift_correction_lat(params) : 1;
-localparam K1_MAX = (1 << LOGK1) - 1;
-localparam K2_MAX = (1 << LOGK2) - 1;
-localparam K3_MAX = (1 << LOGK3) - 1;
+localparam L1_MAX = (1 << LOGL1) - 1;
+localparam L2_MAX = (1 << LOGL2) - 1;
+localparam L3_MAX = (1 << LOGL3) - 1;
 
 /////////////////////////////////////////////////////////////////////////
 
@@ -60,29 +61,29 @@ wire [LOGQ-1:0] CL;
 reg  [LOGQ-1:0] CL_q;
 wire [LOGQ-1:0] CL_mx;
 
-reg  [LOGK1-1:0] K1_q [0:2];
-wire [LOGK1-1:0] K1_mx [0:2];
-reg  [LOGK2-1:0] K2_q [0:2];
-wire [LOGK2-1:0] K2_mx [0:2];
-reg  [LOGK3-1:0] K3_q [0:2];
-wire [LOGK3-1:0] K3_mx [0:2];
+reg  [LOGL1-1:0] L1_q [0:2];
+wire [LOGL1-1:0] L1_mx [0:2];
+reg  [LOGL2-1:0] L2_q [0:2];
+wire [LOGL2-1:0] L2_mx [0:2];
+reg  [LOGL3-1:0] L3_q [0:2];
+wire [LOGL3-1:0] L3_mx [0:2];
 
 
 wire CL1;
 reg  CL1_q [0:3];
 wire CL1_mx[0:3];
 
-wire [LOGQ+K1_MAX-1:0] CL_K1;
-reg  [LOGQ+K1_MAX-1:0] CL_K1_q;
-wire [LOGQ+K1_MAX-1:0] CL_K1_mx;
+wire [LOGQ+L1_MAX-1:0] CL_L1;
+reg  [LOGQ+L1_MAX-1:0] CL_L1_q;
+wire [LOGQ+L1_MAX-1:0] CL_L1_mx;
 
-wire [LOGQ+K2_MAX-1:0] CL_K2;
-reg  [LOGQ+K2_MAX-1:0] CL_K2_q;
-wire [LOGQ+K2_MAX-1:0] CL_K2_mx;
+wire [LOGQ+L2_MAX-1:0] CL_L2;
+reg  [LOGQ+L2_MAX-1:0] CL_L2_q;
+wire [LOGQ+L2_MAX-1:0] CL_L2_mx;
 
-wire [LOGQ+K2_MAX-1:0] CL_K3;
-reg  [LOGQ+K2_MAX-1:0] CL_K3_q;
-wire [LOGQ+K2_MAX-1:0] CL_K3_mx;
+wire [LOGQ+L2_MAX-1:0] CL_L3;
+reg  [LOGQ+L2_MAX-1:0] CL_L3_q;
+wire [LOGQ+L2_MAX-1:0] CL_L3_mx;
 
 wire [LOGQ-1:0] CL_Q;
 reg  [LOGQ-1:0] CL_Q_q;
@@ -96,17 +97,17 @@ wire [2*LOGQ-2:0] T0_Q;
 reg  [2*LOGQ-2:0] T0_Q_q;
 wire [2*LOGQ-2:0] T0_Q_mx;
 
-wire [LOGQ+K1_MAX-1:0] T0_K1;
-reg  [LOGQ+K1_MAX-1:0] T0_K1_q;
-wire [LOGQ+K1_MAX-1:0] T0_K1_mx;
+wire [LOGQ+L1_MAX-1:0] T0_L1;
+reg  [LOGQ+L1_MAX-1:0] T0_L1_q;
+wire [LOGQ+L1_MAX-1:0] T0_L1_mx;
 
-wire [LOGQ+K2_MAX-1:0] T0_K2;
-reg  [LOGQ+K2_MAX-1:0] T0_K2_q;
-wire [LOGQ+K2_MAX-1:0] T0_K2_mx;
+wire [LOGQ+L2_MAX-1:0] T0_L2;
+reg  [LOGQ+L2_MAX-1:0] T0_L2_q;
+wire [LOGQ+L2_MAX-1:0] T0_L2_mx;
 
-wire [LOGQ+K3_MAX-1:0] T0_K3;
-reg  [LOGQ+K3_MAX-1:0] T0_K3_q;
-wire [LOGQ+K3_MAX-1:0] T0_K3_mx;
+wire [LOGQ+L3_MAX-1:0] T0_L3;
+reg  [LOGQ+L3_MAX-1:0] T0_L3_q;
+wire [LOGQ+L3_MAX-1:0] T0_L3_mx;
 
 wire [LOGQ:0] T1;
 reg  [LOGQ:0] T1_q;
@@ -136,37 +137,37 @@ assign CH = C[2*LOGQ-1:LOGQ];
 
 assign CL_mx    = (FF_IN) ? CL_q    : CL;
 assign CH_mx[0] = (FF_IN) ? CH_q[0] : CH;
-assign K1_mx[0] = (FF_IN) ? K1_q[0] : K1;
-assign K2_mx[0] = (FF_IN) ? K2_q[0] : K2;
-assign K3_mx[0] = (FF_IN && USE_K3) ? K3_q[0] : K3;
+assign L1_mx[0] = (FF_IN) ? L1_q[0] : L1;
+assign L2_mx[0] = (FF_IN) ? L2_q[0] : L2;
+assign L3_mx[0] = (FF_IN && USE_L3) ? L3_q[0] : L3;
 
-assign CL_K1_mx = (FF_SHF0) ? CL_K1_q : CL_K1;
-assign CL_K2_mx = (FF_SHF0) ? CL_K2_q : CL_K2;
-assign CL_K3_mx = (FF_SHF0 && USE_K3) ? CL_K3_q : CL_K3;
+assign CL_L1_mx = (FF_SHF0) ? CL_L1_q : CL_L1;
+assign CL_L2_mx = (FF_SHF0) ? CL_L2_q : CL_L2;
+assign CL_L3_mx = (FF_SHF0 && USE_L3) ? CL_L3_q : CL_L3;
 assign CL_Q_mx  = (FF_SHF0) ? CL_Q_q  : CL_Q;
 assign CH_mx[1] = (FF_SHF0) ? CH_q[1] : CH_mx[0];
 assign CL1_mx[0]= (FF_SHF0) ? CL1_q[0] : CL1;
-assign K1_mx[1] = (FF_SHF0) ? K1_q[1] : K1_mx[0];
-assign K2_mx[1] = (FF_SHF0) ? K2_q[1] : K2_mx[0];
-assign K3_mx[1] = (FF_SHF0 && USE_K3) ? K3_q[1] : K3_mx[0];
+assign L1_mx[1] = (FF_SHF0) ? L1_q[1] : L1_mx[0];
+assign L2_mx[1] = (FF_SHF0) ? L2_q[1] : L2_mx[0];
+assign L3_mx[1] = (FF_SHF0 && USE_L3) ? L3_q[1] : L3_mx[0];
 
 assign T0_mx    = (FF_SUB0) ? T0_q    : T0;
 assign CH_mx[2] = (FF_SUB0) ? CH_q[2] : CH_mx[1];
 assign CL1_mx[1]= (FF_SUB0) ? CL1_q[1]: CL1_mx[0];
-assign K1_mx[2] = (FF_SHF0) ? K1_q[2] : K1_mx[1];
-assign K2_mx[2] = (FF_SHF0) ? K2_q[2] : K2_mx[1];
-assign K3_mx[2] = (FF_SHF0 && USE_K3) ? K3_q[2] : K3_mx[1];
+assign L1_mx[2] = (FF_SUB0) ? L1_q[2] : L1_mx[1];
+assign L2_mx[2] = (FF_SUB0) ? L2_q[2] : L2_mx[1];
+assign L3_mx[2] = (FF_SUB0 && USE_L3) ? L3_q[2] : L3_mx[1];
 
-assign T0_K1_mx = (FF_SHF1) ? T0_K1_q : T0_K1;
-assign T0_K2_mx = (FF_SHF1) ? T0_K2_q : T0_K2;
-assign T0_K3_mx = (FF_SHF1 && USE_K3) ? T0_K3_q : T0_K3;
+assign T0_L1_mx = (FF_SHF1) ? T0_L1_q : T0_L1;
+assign T0_L2_mx = (FF_SHF1) ? T0_L2_q : T0_L2;
+assign T0_L3_mx = (FF_SHF1 && USE_L3) ? T0_L3_q : T0_L3;
 assign T0_Q_mx  = (FF_SHF1) ? T0_Q_q  : T0_Q;
 assign CH_mx[3] = (FF_SHF1) ? CH_q[3] : CH_mx[2];
-assign CL1_mx[2]= (FF_SUB0) ? CL1_q[2]: CL1_mx[1];
+assign CL1_mx[2]= (FF_SHF1) ? CL1_q[2]: CL1_mx[1];
 
 assign T1_mx =    (FF_SUB1) ? T1_q    : T1;
 assign CH_mx[4] = (FF_SUB1) ? CH_q[4] : CH_mx[3];
-assign CL1_mx[3]= (FF_SUB0) ? CL1_q[3]: CL1_mx[2];
+assign CL1_mx[3]= (FF_SUB1) ? CL1_q[3]: CL1_mx[2];
 
 assign T2_mx = (FF_SUM) ? T2_q : T2;
 
@@ -179,11 +180,11 @@ assign T2_mx = (FF_SUM) ? T2_q : T2;
 
 assign CL1 = CL_mx[LOGQ-1]; // for carry computation
 
-assign CL_K1 =  CL_mx << K1_mx[0];
-assign CL_K2 =  CL_mx << K2_mx[0];
-assign CL_K3 = (USE_K3) ? (CL_mx << K3_mx[0]) : 0;
+assign CL_L1 =  CL_mx << L1_mx[0];
+assign CL_L2 =  CL_mx << L2_mx[0];
+assign CL_L3 = (USE_L3) ? (CL_mx << L3_mx[0]) : 0;
 assign CL_Q  = (CL_mx << (LOGQ - 1)) - CL_mx;
-assign T0    = ((CL_K1_mx - CL_K2_mx + CL_K3_mx) << M) + CL_Q_mx;
+assign T0    = ((CL_L1_mx - CL_L2_mx + CL_L3_mx) << M) + CL_Q_mx;
 
 /////////////////////////////////////////////////////////////////////////
 
@@ -192,11 +193,11 @@ assign T0    = ((CL_K1_mx - CL_K2_mx + CL_K3_mx) << M) + CL_Q_mx;
 
 /////////////////////////// multiplication by q /////////////////////////
 
-assign T0_K1 =  T0_mx << K1_mx[2];
-assign T0_K2 =  T0_mx << K2_mx[2];
-assign T0_K3 =  (USE_K3) ? (T0_mx << K3_mx[2]) : 0;
+assign T0_L1 =  T0_mx << L1_mx[2];
+assign T0_L2 =  T0_mx << L2_mx[2];
+assign T0_L3 = (USE_L3) ? (T0_mx << L3_mx[2]) : 0;
 assign T0_Q  = (T0_mx << (LOGQ - 1)) + T0_mx;
-assign T1 = (((T0_K1_mx - T0_K2_mx + T0_K3_mx) << M) + T0_Q_mx) >> (LOGQ - 1);
+assign T1    = (((T0_L1_mx - T0_L2_mx + T0_L3_mx) << M) + {1'b0, T0_Q_mx}) >> (LOGQ - 1);
 
 /////////////////////////////////////////////////////////////////////////
 
@@ -259,26 +260,26 @@ always @(posedge clk) begin
     if (FF_IN) begin
         CL_q    <= CL;
         CH_q[0] <= CH;
-        K1_q[0] <= K1;
-        K2_q[0] <= K2;
-        if (USE_K3) begin
-            K3_q[0] <= K3;
+        L1_q[0] <= L1;
+        L2_q[0] <= L2;
+        if (USE_L3) begin
+            L3_q[0] <= L3;
         end
     end
 
     if (FF_SHF0) begin
-        CL_K1_q <= CL_K1;
-        CL_K2_q <= CL_K2;
-        if (USE_K3) begin
-            CL_K3_q <= CL_K3;
+        CL_L1_q <= CL_L1;
+        CL_L2_q <= CL_L2;
+        if (USE_L3) begin
+            CL_L3_q <= CL_L3;
         end
         CL_Q_q  <= CL_Q;
         CH_q[1] <= CH_mx[0];
         CL1_q[0]<= CL1;
-        K1_q[1] <= K1_mx[0];
-        K2_q[1] <= K2_mx[0];
-        if (USE_K3) begin
-            K3_q[1] <= K3_mx[0];
+        L1_q[1] <= L1_mx[0];
+        L2_q[1] <= L2_mx[0];
+        if (USE_L3) begin
+            L3_q[1] <= L3_mx[0];
         end
     end
 
@@ -286,18 +287,18 @@ always @(posedge clk) begin
         T0_q    <= T0;
         CH_q[2] <= CH_mx[1];
         CL1_q[1]<= CL1_mx[0];
-        K1_q[2] <= K1_mx[1];
-        K2_q[2] <= K2_mx[1];
-        if (USE_K3) begin
-            K3_q[2] <= K3_mx[1];
+        L1_q[2] <= L1_mx[1];
+        L2_q[2] <= L2_mx[1];
+        if (USE_L3) begin
+            L3_q[2] <= L3_mx[1];
         end
     end
 
     if (FF_SHF1) begin
-        T0_K1_q <= T0_K1;
-        T0_K2_q <= T0_K2;
-        if (USE_K3) begin
-            T0_K3_q <= T0_K3;
+        T0_L1_q <= T0_L1;
+        T0_L2_q <= T0_L2;
+        if (USE_L3) begin
+            T0_L3_q <= T0_L3;
         end
         T0_Q_q  <= T0_Q;
         CH_q[3] <= CH_mx[2];
--- src/monty/montgomery_shift_tb.sv
+++ src/monty/montgomery_shift_tb.sv
@@ -12,8 +12,10 @@ module montgomery_shift_tb
         parameter           USE_L3  = 1       ,
         parameter           LOGL3   = 5       ,
         parameter           FF_IN   = 1       ,
-        parameter           FF_SHF  = 1       ,
-        parameter           FF_SUB  = 1       ,
+        parameter           FF_SHF0 = 1       ,
+        parameter           FF_SUB0 = 1       ,
+        parameter           FF_SHF1 = 1       ,
+        parameter           FF_SUB1 = 1       ,
         parameter           FF_SUM  = 1       ,
         parameter           FF_OUT  = 1       ,
         parameter           HP      = 5       ,
@@ -22,10 +24,11 @@ module montgomery_shift_tb
         parameter string    FN_T    = "T.txt"
     );
 
-localparam LOGC = LOGQ*2;
+localparam K = LOGQ*2;
 localparam LOGT = montgomery_shift_inst.LOGT;
 
 reg clk;
+reg rst;
 
 localparam FP = (2*HP);
 
@@ -43,7 +46,7 @@ reg  [LOGQH-1:0] qH;
 reg  [LOGQH-1:0] Q;
 wire [LOGT -1:0] T;
 reg  [LOGT -1:0] T_;
-reg  [LOGC -1:0] C;
+reg  [K    -1:0] C;
 reg  [LOGL1-1:0] L1;
 reg  [LOGL2-1:0] L2;
 reg  [LOGL3-1:0] L3;
@@ -55,8 +58,10 @@ montgomery_shift
         .LOGQH  (LOGQH  ),
         .CORRECT(CORRECT),
         .FF_IN  (FF_IN  ),
-        .FF_SUB (FF_SUB ),
-        .FF_SHF (FF_SHF ),
+        .FF_SUB0(FF_SUB0),
+        .FF_SHF0(FF_SHF0),
+        .FF_SUB1(FF_SUB1),
+        .FF_SHF1(FF_SHF1),
         .FF_SUM (FF_SUM ),
         .FF_OUT (FF_OUT ),
         .USE_L3 (USE_L3 ),
@@ -67,6 +72,7 @@ montgomery_shift
 montgomery_shift_inst 
     (
         .clk(clk),
+        .rst(rst),
         .qH (qH),
         .C  (C ),
         .L1 (L1),
@@ -151,8 +157,11 @@ initial begin
     $display("Simulation started.");
 
     clk = 1'b0;
+    rst = 1'b0;
     #FP;
+    rst = 1'b1;
     #FP;
+    rst = 1'b0;
     #(HP);
     #(1);
     // Initialize inputs
