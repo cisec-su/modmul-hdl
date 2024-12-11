9d46692a6ee1179c43d09e08283aefa23e9a76f0
berenaydogan <63881615+berenaydogan@users.noreply.github.com>
2024-12-11 17:18:51 +0300

    Remove redundant lines (#5)

--- src/modop/modmul.sv
+++ src/modop/modmul.sv
@@ -26,11 +26,8 @@ module modmul
     localparam W    = LOGQ - LOGQH;
     localparam LOGT = (CORRECT) ? LOGQ : LOGQ + 1;
     
-    localparam intmul_params_t intmul_params = {FF_IN, FF_MUL, FF_OUT, FF_CSA, USE_CSA};
-    localparam wlm_mixed_params_t wlm_mixed_params = {LOGQ, LOGQH, CORRECT, FF_IN, FF_SUB, FF_MUL, FF_SUM, FF_OUT};
-    localparam wlm_params_t wlm_params = {W, LOGQ, LOGQH, CORRECT, FF_IN, FF_SUB, FF_MUL, FF_SUM, FF_OUT};
-    localparam modmul_params_t modmul_params = {W, LOGQ, LOGQH, CORRECT, FF_IN, FF_SUB, FF_MUL, FF_SUM, FF_OUT};
-    localparam LAT = modmul_lat(intmul_params, wlm_mixed_params, wlm_params, modmul_params);
+    localparam modmul_params_t modmul_params = {W, LOGQ, LOGQH, CORRECT, FF_IN, FF_MUL, FF_SUM, FF_SUB, FF_OUT, USE_CSA, FF_CSA};
+    localparam LAT = modmul_lat(modmul_params);
 
     localparam USE_WLM_MIXED = (LOGQH < `DSP_B_U) ? 1 : 0;
 
@@ -41,7 +38,7 @@ module modmul
         .LOGB(LOGQ),
         .FF_IN(FF_IN),
         .FF_MUL(FF_MUL),
-        .FF_OUT(FF_OUT),
+        .FF_OUT(1),
         .USE_CSA(USE_CSA),
         .FF_CSA(FF_CSA),
         .MORE_DSP(MORE_DSP),
@@ -59,7 +56,7 @@ module modmul
                 .LOGQ(LOGQ),
                 .LOGQH(LOGQH),
                 .CORRECT(CORRECT),
-                .FF_IN(FF_IN),
+                .FF_IN(0),
                 .FF_SUM(FF_SUM),
                 .FF_MUL(FF_MUL),
                 .FF_SUB(FF_SUB),
@@ -75,7 +72,7 @@ module modmul
                 .LOGQ(LOGQ),
                 .LOGQH(LOGQH),
                 .CORRECT(CORRECT),
-                .FF_IN(FF_IN),
+                .FF_IN(0),
                 .FF_SUM(FF_SUM),
                 .FF_MUL(FF_MUL),
                 .FF_SUB(FF_SUB),
--- src/modop/modmul.svh
+++ src/modop/modmul.svh
@@ -7,10 +7,13 @@
 `include "wlm_mixed.svh"
 
 typedef struct packed {
-    int W, LOGQ, LOGQH, CORRECT, FF_IN, FF_SUB, FF_MUL, FF_SUM, FF_OUT;
+    int W, LOGQ, LOGQH, CORRECT, FF_IN, FF_MUL, FF_SUM, FF_SUB, FF_OUT, USE_CSA, FF_CSA;
 } modmul_params_t;
 
-function int modmul_lat(input intmul_params_t intmul_params, input wlm_mixed_params_t wlm_mixed_params, input wlm_params_t wlm_params, input modmul_params_t modmul_params);
+function int modmul_lat(input modmul_params_t modmul_params);
+    wlm_mixed_params_t wlm_mixed_params = {modmul_params.LOGQ, modmul_params.LOGQH, modmul_params.CORRECT, modmul_params.FF_IN, modmul_params.FF_SUB, modmul_params.FF_MUL, modmul_params.FF_SUM, modmul_params.FF_OUT};
+    wlm_params_t wlm_params = {modmul_params.W, modmul_params.LOGQ, modmul_params.LOGQH, modmul_params.CORRECT, modmul_params.FF_IN, modmul_params.FF_SUB, modmul_params.FF_MUL, modmul_params.FF_SUM, modmul_params.FF_OUT};
+    intmul_params_t intmul_params = {modmul_params.FF_IN, modmul_params.FF_MUL, modmul_params.FF_OUT, modmul_params.FF_CSA, modmul_params.USE_CSA};
     modmul_lat = intmul_lat(intmul_params) + ((modmul_params.LOGQH < `DSP_B_U) ? wlm_mixed_lat(wlm_mixed_params) : wlm_lat(wlm_params));
 endfunction
 
